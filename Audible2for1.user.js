f82c419b3e8ebbaa2b9c1767d5d7663a6eb64745
David Schor <David@schor.dk>
2024-01-07 12:33:48 +0100

    Update Audible2for1.user.js

--- Audible2for1.user.js
+++ Audible2for1.user.js
@@ -38,36 +38,4 @@
     divButton.addEventListener('click', () => {
         document.querySelectorAll('.adblAddToWishlistButton:not(.bc-hidden)').forEach(element => {if(element.closest('li').innerHTML.indexOf("Shelf") === -1){
                                                                                       element.closest('li').remove()}}
-    
-    // Play in tab in library. Credit to https://github.com/hnprashanth/audible-play-in-tab                                                                                 )})
-    var asins = []
-  $("input[name='asin']").each(function () {
-    const asin = $(this).val()
-    if (asins.indexOf(asin) == -1) {
-      asins.push(asin)
-    }
-  })
-
-    // Play in tab in library. Credit to https://github.com/hnprashanth/audible-play-in-tab
-  $('.adbl-library-item').each(function (index) {
-    const thisAsin = asins[index]
-    console.log(asins[index])
-    const bookUrl = 'https://' + document.domain + '/webplayer?asin=' + thisAsin
-    $(this).after(
-      "<a href='" +
-        bookUrl +
-        "' target='_blank'><button>Play in Tab</button></a>",
-    )
-  })
-
-  // Play in tab on the page of the book. Inspired by https://github.com/hnprashanth/audible-play-in-tab
-  $("button[name='playButton']").each(function () {
-    const asin = $(this).attr('asin')
-    const bookUrl = 'https://' + document.domain + '/webplayer?asin=' + asin
-    $(this).parent().after(
-      "<a href='" +
-        bookUrl +
-        "' target='_blank'><button>Play in Tab</button></a>",
-    )
-  })
 }())
